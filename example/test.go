03159a8573eda3bb82878f8585a90f62ca790d73
郭玉朝 <2788828128@qq.com>
2018-09-10 00:00:45 +0800

    框架启动logo,cli参数验证

---
+++ example/test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	//c := make(chan os.Signal, 1)
+	//signal.Notify(c, os.Interrupt, os.Kill)
+	//
+	//who := "Alice "
+	//if len(os.Args) != 1 {
+	//	log.Printf("%v", "错误的cli参数(Missing cli parameters)")
+	//}
+	//fmt.Println("Good Morning", who)
+	//
+	//
+	//sign := <-c
+	//log.Println("已退出microSocket框架(Quit the microSocket framework.),信号量:",sign)
+
+
+}
